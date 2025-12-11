107f3ecedfcf04573190e866e05783acfc889444
ocque41 <73175248+ocque41@users.noreply.github.com>
2025-12-11 22:41:10 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,14 @@ import { withWorkflow } from "workflow/next";
 const nextConfig: NextConfig = {
   /* config options here */
   transpilePackages: ['workflow'],
+  webpack: (config, { webpack }) => {
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        React: 'react',
+      })
+    );
+    return config;
+  },
 };
 
 export default withWorkflow(nextConfig);
